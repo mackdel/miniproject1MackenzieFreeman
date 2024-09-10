ab48ea9b0e886333390b36b9740e3b5569dbe941
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-10 15:44:18 -0400

    Setup yfinance to pull 5 tickers.

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 # INF601 - Advanced Programming in Python
 # Mackenzie Freeman
 # Mini Project 1
+
+import pprint
+import yfinance as yf
+
+mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
+# marvell is MRVL
+# intel is INTC
+# amazon is #AMZN
+
+mydata = {}
+
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    mydata[ticker] = {'ticker': ticker,
+                      'dailyHigh': result.info['dayHigh']
+                      }
+    print(f"Ticker: {ticker} \tDaily High: {result.info['dayHigh']}")
+
+# get all stock info
+#pprint.pprint(msft.info)
+
+# get historical market data
+#hist = msft.history(period="5d")
+
+#pprint.pprint(hist)
