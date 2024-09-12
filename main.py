9c2fd04e1fb099e79fbac3bcb4f166fc413aae95
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-12 15:47:32 -0400

    Changed stock tickers.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # INF601 - Advanced Programming in Python
 # Mackenzie Freeman
 # Mini Project 1
+
 import os
 import yfinance as yf
 from datetime import datetime, timedelta
@@ -11,10 +12,7 @@ import matplotlib.pyplot as plt
 os.makedirs("charts", exist_ok=True)
 
 # 5 stock tickers
-mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
-# marvell is MRVL
-# intel is INTC
-# amazon is #AMZN
+mytickers = ["MSFT", "MRVL", "INTC", "AMZN", "AMC"]
 
 # Get today's date
 today = datetime.now()
@@ -33,13 +31,13 @@ for ticker in mytickers:
     # Convert array to an array in NumPy and plot graph
     if len(last10days) == 10:
         myarray = np.array(last10days)
-        max_price = myarray.max() + (myarray.max()*.05)
-        min_price = myarray.min() - (myarray.min()*.05)
+        max_price = myarray.max() + (myarray.max() * .05)
+        min_price = myarray.min() - (myarray.min() * .05)
         plt.plot(myarray)
         plt.xlabel('Days Ago')
         plt.ylabel('Closing Price')
         plt.axis((9, 0, min_price, max_price))
-        plt.title(f"{ticker} last 10 Closing Prices")
+        plt.title(f"{ticker} Last 10 Closing Prices")
         plt.savefig(f"charts/{ticker}.png")
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
