50e73ae28907b80e4ba748177c8c2a3a9135345f
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-12 15:20:01 -0400

    Saved charts as PNG files under charts folder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # INF601 - Advanced Programming in Python
 # Mackenzie Freeman
 # Mini Project 1
-
+import os
 import pprint
 import yfinance as yf
 from datetime import datetime, timedelta
@@ -29,17 +29,16 @@ for ticker in mytickers:
     last10days = []
     for date in hist['Close'][:11]:
         last10days.append(date)
-
     # Convert array to an array in NumPy and plot graph
     if len(last10days) == 10:
         myarray = np.array(last10days)
         max_price = myarray.max() + (myarray.max()*.05)
-        min_price = myarray.min() - (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.min()*.05)
         plt.plot(myarray)
         plt.xlabel('Days Ago')
         plt.ylabel('Closing Price')
-        plt.axis((9, 1, min_price, max_price))
+        plt.axis((9, 0, min_price, max_price))
         plt.title(f"{ticker} last 10 Closing Prices")
-        plt.show()
+        plt.savefig(f"charts/{ticker}.png")
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
