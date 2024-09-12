6b2a8170abe368f8f81ab0da29ff33106b536529
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-12 15:11:15 -0400

    Add matplotlib. Converted close costs to a numpy array. Created and displayed graphs

--- main.py
+++ main.py
@@ -4,25 +4,42 @@
 
 import pprint
 import yfinance as yf
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
 
+# 5 stock tickers
 mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
 # marvell is MRVL
 # intel is INTC
 # amazon is #AMZN
 
-mydata = {}
+# Get today's date
+today = datetime.now()
 
+# Calculate the date 10 days ago
+ten_days_ago = today - timedelta(days=14)
+
+mytickers.sort()
 for ticker in mytickers:
+    # Collect the closing price of the stock tickers
+    # for the last 10 trading days.
     result = yf.Ticker(ticker)
-    mydata[ticker] = {'ticker': ticker,
-                      'dayHigh': result.info['dayHigh']
-                      }
-    print(f"Ticker: {ticker} \tDaily High: {result.info['dayHigh']}")
+    hist = result.history(start=ten_days_ago, end=today)
+    last10days = []
+    for date in hist['Close'][:11]:
+        last10days.append(date)
 
-# get all stock info
-#pprint.pprint(msft.info)
-
-# get historical market data
-#hist = msft.history(period="5d")
-
-#pprint.pprint(hist)
+    # Convert array to an array in NumPy and plot graph
+    if len(last10days) == 10:
+        myarray = np.array(last10days)
+        max_price = myarray.max() + (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.max()*.05)
+        plt.plot(myarray)
+        plt.xlabel('Days Ago')
+        plt.ylabel('Closing Price')
+        plt.axis((9, 1, min_price, max_price))
+        plt.title(f"{ticker} last 10 Closing Prices")
+        plt.show()
+    else:
+        print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
